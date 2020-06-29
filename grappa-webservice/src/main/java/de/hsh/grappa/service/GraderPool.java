9870f941fb2a17b4d8f907a7c69af96e76e6dbc4
schmendrik <apaulreiser@gmail.com>
2020-06-29 13:40:06 +0200

    Use is-internal-error responses for timed out and failed grading processes
    Also use the JaxbExecutor for fixing CompletableFuture not being able to load Jaxb classes

--- grappa-webservice/src/main/java/de/hsh/grappa/service/GraderPool.java
+++ grappa-webservice/src/main/java/de/hsh/grappa/service/GraderPool.java
@@ -49,7 +49,6 @@ public class GraderPool {
 
     public GraderPool(GraderConfig graderConfig, GraderPoolManager graderManager) throws Exception {
         this.graderConfig = graderConfig;
-        log.info("Loading and initializing grader backend plugin {}...", graderConfig.getId());
         this.backendPlugin = loadBackendPlugin(graderConfig);
         log.debug("Using grader '{}' with {} concurrent instances.",
             graderConfig.getId(), graderConfig.getConcurrent_grading_processes());
@@ -65,7 +64,7 @@ public class GraderPool {
      *
      * @return Frue, if a grade process has started. False, if all workers are busy
      */
-    public boolean tryGrade() {
+    public boolean tryGrade(/*boolean async*/) {
         // We need to acquire the semaphore this soon already, since we
         // pop a submission from the queue in the next step and commit
         // ourselves to grading it. Otherwise, we'd have to put the submission
@@ -79,14 +78,31 @@ public class GraderPool {
                     releaseSemaphore = false; // starting another thread that will release the semaphore eventually
                     log.info("[GraderId: '{}', GradeProcessId: '{}']: Starting grading process...",
                         graderConfig.getId(), queuedSubm.getGradeProcId());
-                    CompletableFuture.supplyAsync(() -> {
-                        return runGradingProcess(queuedSubm);
-                    }).thenAccept(resp -> {
-                        processProformaResponseResult(resp, queuedSubm.getGradeProcId());
-                    }).thenRun(() -> {
-                        // Grading slot has become free, try grading if there's anything queued.
-                        tryGrade();
-                    });
+
+                    if (true/*async*/) {
+                        CompletableFuture.supplyAsync(() -> {
+                            return runGradingProcess(queuedSubm);
+                        }, getJaxbExecutor()).thenAccept(resp -> {
+                            cacheProformaResponseResult(resp, queuedSubm.getGradeProcId());
+                        }).thenRun(() -> {
+                            // Grading slot has become free, try grading if there's anything queued.
+                            //tryGrade(false); // reuse future's async thread
+                            tryGrade();
+                        });
+                    } /*else {
+                        var future = CompletableFuture.supplyAsync(() -> {
+                            return runGradingProcess(queuedSubm);
+                        }).thenAccept(resp -> {
+                            processProformaResponseResult(resp, queuedSubm.getGradeProcId());
+                        });
+                        // No timeout. Timeout and subsequent cancellation
+                        // handled in runGradingProcess()
+                        future.get();
+                        CompletableFuture.runAsync(() -> {
+                            // Grading slot has become free, try grading if there's anything queued.
+                            tryGrade(false); // reuse future's async thread
+                        });
+                    }*/
                 } else
                     log.debug("[GraderID: '{}']: This grader's submission queue is empty.", graderConfig.getId());
             } catch (Exception e) {
@@ -104,77 +120,83 @@ public class GraderPool {
     }
 
     public ProformaResponse runGradingProcess(QueuedSubmission subm) {
-        FutureTask<ProformaResponse> futureTask = null;
         try {
-            log.debug("GRADE START: {}", subm.getGradeProcId());
-            futureTask = new FutureTask<ProformaResponse>(() -> {
-                return backendPlugin.grade(subm.getSubmission());
-            });
-            gpMap.put(subm.getGradeProcId(), futureTask);
-            new Thread(futureTask).start();
-            ProformaResponse resp = futureTask.get(graderConfig.getTimeout_seconds(), TimeUnit.SECONDS);
-            log.info("[GraderId: '{}', GradeProcessId: '{}']: Grading process exited.",
-                graderConfig.getId(), subm.getGradeProcId());
-            if (null != resp) {
-                totalGradingProcessesSucceeded.incrementAndGet();
-                log.info("[GraderId: '{}', GradeProcessId: '{}']: Grading process finished successfully.",
+            FutureTask<ProformaResponse> futureTask = null;
+            int timeoutSeconds = graderConfig.getTimeout_seconds();
+            try {
+                log.debug("GRADE START: {}", subm.getGradeProcId());
+                futureTask = new FutureTask<ProformaResponse>(() -> {
+                    return backendPlugin.grade(subm.getSubmission());
+                });
+                gpMap.put(subm.getGradeProcId(), futureTask);
+                new Thread(futureTask).start();
+                ProformaResponse resp = futureTask.get(timeoutSeconds, TimeUnit.SECONDS);
+                log.info("[GraderId: '{}', GradeProcessId: '{}']: Grading process exited.",
                     graderConfig.getId(), subm.getGradeProcId());
-                return resp;
+                if (null != resp) {
+                    totalGradingProcessesSucceeded.incrementAndGet();
+                    log.info("[GraderId: '{}', GradeProcessId: '{}']: Grading process finished successfully.",
+                        graderConfig.getId(), subm.getGradeProcId());
+                    return resp;
+                }
+                throw new NoResultGraderExecption("Grader did not supply a proforma response.");
+            } catch (ExecutionException | NoResultGraderExecption e) { // BackendPlugin threw an exception
+                totalGradingProcessesFailed.incrementAndGet();
+                String errorMessage = String.format("[GraderId: '%s', GradeProcessId: '%s']: Grading process failed " +
+                        "with error: %s", graderConfig.getId(), subm.getGradeProcId(), e.getMessage());
+                log.error(errorMessage);
+                log.error(ExceptionUtils.getStackTrace(e));
+                return ProformaV201ResponseGenerator.createInternalErrorResponse(errorMessage);
+            } catch (TimeoutException e) {
+                totalGradingProcessesTimedOut.incrementAndGet();
+                log.info("[GraderId: '{}', GradeProcessId: '{}']: Grader timed out after {} seconds. Trying to cancel " +
+                        "grading process...",
+                    graderConfig.getId(), subm.getGradeProcId(), timeoutSeconds);
+                futureTask.cancel(true);
+                log.info("[GraderId: '{}', GradeProcessId: '{}']: Grading process has been cancelled after timing out.",
+                    graderConfig.getId(), subm.getGradeProcId());
+                // Don't increment totalGradingProcessesCancelled, since the cancellation was due to a timeout.
+                // How do we know this timeout was due to the grader and not the student submission???
+                return ProformaV201ResponseGenerator.createInternalErrorResponse(String.format("Grader '%s' timed out " +
+                    "after %d seconds.", graderConfig.getId(), timeoutSeconds));
+            } catch (CancellationException e) {
+                totalGradingProcessesCancelled.incrementAndGet();
+                log.info("[GraderId: '{}', GradeProcessId: '{}']: Grading process cancelled.",
+                    graderConfig.getId(), subm.getGradeProcId());
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                log.info("[GraderId: '{}', GradeProcessId: '{}']: Parent thread of grade process interrupted. Trying to " +
+                    "cancel grading process...", graderConfig.getId(), subm.getGradeProcId());
+                futureTask.cancel(true);
+                // Treat this as cancellation, a direct result of the interrupt
+                totalGradingProcessesCancelled.incrementAndGet();
+                log.debug("[GraderId: '{}', GradeProcessId: '{}']: Grading process has been cancelled after  parent " +
+                        "thread interruption.",
+                    graderConfig.getId(), subm.getGradeProcId(), timeoutSeconds);
+                // TODO: return errorResponse with is-inernal-error?
+            } catch (Exception e) { // anything else unpredictable
+                totalGradingProcessesFailed.incrementAndGet();
+                log.error("[GraderId: '{}', GradeProcessId: '{}']: Grading process failed with error: {}",
+                    graderConfig.getId(), subm.getGradeProcId(), e.getMessage());
+                log.error(ExceptionUtils.getStackTrace(e));
+                // TODO: return errorResp with is-internal-error=true and errorMsg=e.getMessage()
+                return null;
+            } finally {
+                totalGradingProcessesExecuted.incrementAndGet(); // finished one way or the other
+                log.debug("GRADE ENDE: {}", subm.getGradeProcId());
+                semaphore.release();
+                gpMap.remove(subm.getGradeProcId());
+                log.debug("Grader '{}': semaphore released, {} left", graderConfig.getId(),
+                    semaphore.availablePermits());
             }
-            throw new NoResultGraderExecption("Grader did not supply a proforma response.");
-        } catch (ExecutionException | NoResultGraderExecption e) { // BackendPlugin threw an exception
-            totalGradingProcessesFailed.incrementAndGet();
-            log.error("[GraderId: '{}', GradeProcessId: '{}']: Grading process failed with error: {}",
-                graderConfig.getId(), subm.getGradeProcId(), e.getMessage());
+        } catch (Exception e) {
+            log.error("Code hadling grading process exceptions failed with: {}", e.getMessage());
             log.error(ExceptionUtils.getStackTrace(e));
-            // TODO: maybe create a is-internal-error response with the grader log as feedback? What about
-            //  timeouts though, it could be the student's code that timed out.
-        } catch (TimeoutException e) {
-            totalGradingProcessesTimedOut.incrementAndGet();
-            log.info("[GraderId: '{}', GradeProcessId: '{}']: Grader timed out after {} seconds. Trying to cancel " +
-                    "grading process...",
-                graderConfig.getId(), subm.getGradeProcId(), graderConfig.getTimeout_seconds());
-            futureTask.cancel(true);
-            log.info("[GraderId: '{}', GradeProcessId: '{}']: Grading process has been cancelled after timing out.",
-                graderConfig.getId(), subm.getGradeProcId(), graderConfig.getTimeout_seconds());
-            // Don't increment totalGradingProcessesCancelled, since the cancellation was due to a timeout.
-            // TODO: return resp with is-internal-true and errorMessage=timeout.
-            // How do we know this timeout was due to the grader and not the student submission???
-            return null;
-        } catch (CancellationException e) {
-            totalGradingProcessesCancelled.incrementAndGet();
-            log.info("[GraderId: '{}', GradeProcessId: '{}']: Grading process cancelled.",
-                graderConfig.getId(), subm.getGradeProcId());
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            log.info("[GraderId: '{}', GradeProcessId: '{}']: Parent thread of grade process interrupted. Trying to " +
-                "cancel grading process...", graderConfig.getId(), subm.getGradeProcId());
-            futureTask.cancel(true);
-            // Treat this as cancellation, a direct result of the interrupt
-            totalGradingProcessesCancelled.incrementAndGet();
-            log.debug("[GraderId: '{}', GradeProcessId: '{}']: Grading process has been cancelled after  parent " +
-                    "thread interruption.",
-                graderConfig.getId(), subm.getGradeProcId(), graderConfig.getTimeout_seconds());
-            // TODO: return errorResponse with is-inernal-error?
-        } catch (Exception e) { // anything else unpredictable
-            totalGradingProcessesFailed.incrementAndGet();
-            log.error("[GraderId: '{}', GradeProcessId: '{}']: Grading process failed with error: {}",
-                graderConfig.getId(), subm.getGradeProcId(), e.getMessage());
-            log.error(ExceptionUtils.getStackTrace(e));
-            // TODO: return errorResp with is-internal-error=true and errorMsg=e.getMessage()
-            return null;
-        } finally {
-            totalGradingProcessesExecuted.incrementAndGet(); // finished one way or the other
-            log.debug("GRADE ENDE: {}", subm.getGradeProcId());
-            semaphore.release();
-            gpMap.remove(subm.getGradeProcId());
-            log.debug("Grader '{}': semaphore released, {} left", graderConfig.getId(),
-                semaphore.availablePermits());
         }
         return null;
     }
 
-    private void processProformaResponseResult(ProformaResponse resp, String gradeProcId) {
+    private void cacheProformaResponseResult(ProformaResponse resp, String gradeProcId) {
         if (null != resp) {
             log.debug("[Grader '{}']: Caching response: {}", graderConfig.getId(), resp);
             GrappaServlet.redis.setResponse(gradeProcId, resp);
@@ -217,10 +239,11 @@ public class GraderPool {
     }
 
     private BackendPlugin loadBackendPlugin(GraderConfig grader) throws Exception {
+        log.info("Loading and initializing grader '{}' from file '{}'...",
+            grader.getId(), grader.getClass_path());
         BackendPlugin bp = new ClassLoaderHelper<BackendPlugin>().LoadClass(grader.getClass_path(),
             grader.getClass_name(),
             BackendPlugin.class);
-        log.info("Grader '{}' loaded. Initializing grader...", grader.getId());
         try (InputStream is = new FileInputStream(new File(grader.getConfig_path()))) {
             Properties props = new Properties();
             props.load(is);
@@ -247,5 +270,11 @@ public class GraderPool {
 
     public long getTotalGradingProcessesTimedOut() {
         return totalGradingProcessesTimedOut.get();
+    }
+
+    private ForkJoinPool getJaxbExecutor() {
+        JaxbForkJoinWorkerThreadFactory threadFactory = new JaxbForkJoinWorkerThreadFactory();
+        int parallelism = Math.min(0x7fff /* copied from ForkJoinPool.java */, Runtime.getRuntime().availableProcessors());
+        return new ForkJoinPool(parallelism, threadFactory, null, false);
     }
 }
