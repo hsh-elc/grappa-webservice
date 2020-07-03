3065ad8dae8135037fc7a121a73e5868a2fa3a7b
Paul Reiser <apaulreiser@gmail.com>
2020-07-03 20:55:48 +0200

    Add illegal argument exception mapper
    For mapping illegal argument exceptions to bad request exceptions

---
+++ grappa-webservice/src/main/java/de/hsh/grappa/rest/IllegalArgumentExceptionMapper.java
@@ -0,0 +1,27 @@
+package de.hsh.grappa.rest;
+
+import de.hsh.grappa.utils.Json;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class IllegalArgumentExceptionMapper implements
+    ExceptionMapper<IllegalArgumentException> {
+    private static Logger log = LoggerFactory.getLogger(AllExceptionMapper.class);
+
+    @Override
+    @Produces(MediaType.APPLICATION_JSON + "; charset=utf-8")
+    public Response toResponse(IllegalArgumentException e) {
+        log.error(e.getMessage());
+        log.error(ExceptionUtils.getStackTrace(e));
+        return Response.status(Response.Status.BAD_REQUEST)
+            .entity(Json.createJsonExceptionMessage(e)).build();
+    }
+}
