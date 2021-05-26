c4d208179cf234d7fbac54aa8a328662c616b73c
Paul Reiser <apaulreiser@gmail.com>
2021-05-26 16:48:55 +0200

    Use Apache's module for handling ZIP archive
    Use Apache's ZipArchiveInputStream in favor of Java's native ZipInputStream, which has pretty bad (besically non-existent) error info when it comes to handling corrupt ZIP archive files.

--- grappa-backend-plugin-api/src/main/java/de/hsh/grappa/utils/Zip.java
+++ grappa-backend-plugin-api/src/main/java/de/hsh/grappa/utils/Zip.java
@@ -1,14 +1,13 @@
 package de.hsh.grappa.utils;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
+import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream;
+
+import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
 
 public class Zip {
     public static String getTextFileContentFromZip(byte[] zipBytes, String fileName, Charset charset) throws Exception {
@@ -29,17 +28,23 @@ public class Zip {
     }
 
     public static byte[] getFileFromZip(InputStream zipStream, String fileName) throws Exception {
-        //fileName = fileName.replace('\\', '/');
         Path filePath = Paths.get(fileName);
-        //System.out.println("Looking for " + fileName + " in ZIP...");
+        //System.out.println("Looking for " + filePath + " in ZIP...");
         boolean fileNotFound = true;
-        try (ZipInputStream zip = new ZipInputStream(zipStream)) {
+
+        try (ZipArchiveInputStream zip = new ZipArchiveInputStream(new BufferedInputStream(zipStream))) {
             ByteArrayOutputStream out = new ByteArrayOutputStream();
             ZipEntry ze;
-            while (null != (ze = zip.getNextEntry())) {
-                Path zePath = Paths.get(ze.getName());
+
+            ZipArchiveEntry entry = null;
+            while (null != (entry = zip.getNextZipEntry())) {
+                if (entry == null) {
+                    break;
+                }
+
+                Path zePath = Paths.get(entry.getName());
                 //System.out.println("Checking file/derectory in ZIP: " + zePath);
-                if(zePath.equals(filePath)) {
+                if (zePath.equals(filePath)) {
                     fileNotFound = false;
                     byte[] buffer = new byte[10000000]; // 10Mb file
                     int len;
@@ -50,27 +55,42 @@ public class Zip {
                     break;
                 }
             }
-            zip.close(); // TODO is auto closed, actually
-            if(fileNotFound)
+
+            if (fileNotFound)
                 throw new FileNotFoundException(String.format("File '%s' not found in ZIP archive.", filePath));
             return out.toByteArray();
-
-        } catch (Exception e) {
+        } catch (Throwable e) {
+            System.err.println(e);
             throw e;
         }
     }
 
+// This code (Java's native ZipInputStream) works just fine.
+// However its error info on exceptions is pretty much non-existent
+// when it comes to handling invalid ZIP files.
+//
 //    public static byte[] getFileFromZip(InputStream zipStream, String fileName) throws Exception {
+//        System.out.println("Entering getFileFromZip");
 //        //fileName = fileName.replace('\\', '/');
 //        Path filePath = Paths.get(fileName);
-//        System.out.println("Looking for " + fileName + " in ZIP...");
+//        System.out.println("Looking for " + filePath + " in ZIP...");
 //        boolean fileNotFound = true;
 //        try (ZipInputStream zip = new ZipInputStream(zipStream)) {
-//            var out = new ByteArrayOutputStream();
+//            ByteArrayOutputStream out = new ByteArrayOutputStream();
 //            ZipEntry ze;
-//            while (null != (ze = zip.getNextEntry())) {
+//            ze = zip.getNextEntry();
+//            //while (null != (ze = zip.getNextEntry())) {
+//            do {
+//                if(ze == null) {
+//                    System.out.println("ze null.. checking next entry...");
+//                    ze = zip.getNextEntry();
+//                    System.out.println("and: " + ze);
+//                    break;
+//                }
+//
 //                Path zePath = Paths.get(ze.getName());
-//                System.out.println("Checking file/derectory in ZIP: " + zePath);
+//                //System.out.println("Checking file/derectory in ZIP: " + zePath);
+//                System.out.println(String.format("'%s'=='%s' -> %s", filePath, zePath, zePath.equals(filePath)));
 //                if(zePath.equals(filePath)) {
 //                    fileNotFound = false;
 //                    byte[] buffer = new byte[10000000]; // 10Mb file
@@ -81,13 +101,14 @@ public class Zip {
 //                    out.close();
 //                    break;
 //                }
-//            }
-//            zip.close(); // TODO is auto closed, actually
+//            } while (null != (ze = zip.getNextEntry()));
+//            //zip.close(); // TODO is auto closed, actually
 //            if(fileNotFound)
 //                throw new FileNotFoundException(String.format("File '%s' not found in ZIP archive.", filePath));
 //            return out.toByteArray();
 //
-//        } catch (Exception e) {
+//        } catch (Throwable e) {
+//            System.err.println(e);
 //            throw e;
 //        }
 //    }
