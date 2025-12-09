e57758479afce8876b79b2114db0ae39b28b59f8
Robert Garmann <robert.garmann@hs-hannover.de>
2025-12-09 09:50:27 +0100

    WIP Issue 238

--- mvnInstallProformaDependenciesFromGithub.sh
+++ mvnInstallProformaDependenciesFromGithub.sh
@@ -2,64 +2,111 @@
 
 set -e # exit on first error
 
-PFLIBVER=0.4.0
-GHURL="https://github.com/hsh-elc/proforma/releases/download"
+MVNOPTS=""
+CURLOPTS=""
+VERBOSE="1"
+PFLIBVER="$(mvn help:evaluate -Dexpression=proforma.version -q -DforceStdout)"
+TMPDIR=/tmp
 
-# Jars to be downloaded and installed:
-declare -A arrayDownloads=(\
-  [proforma-${PFLIBVER}.pom]="${GHURL}/v${PFLIBVER}/proforma-${PFLIBVER}.pom" \
-  [proformaxml-${PFLIBVER}.jar]="${GHURL}/v${PFLIBVER}/proformaxml-${PFLIBVER}.jar" \
-  [proformaxml-2-1-${PFLIBVER}.jar]="${GHURL}/v${PFLIBVER}/proformaxml-2-1-${PFLIBVER}.jar"  \
-  [proformautil-${PFLIBVER}.jar]="${GHURL}/v${PFLIBVER}/proformautil-${PFLIBVER}.jar"  \
-  [proformautil-2-1-${PFLIBVER}.jar]="${GHURL}/v${PFLIBVER}/proformautil-2-1-${PFLIBVER}.jar"  \
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    CYGWIN*)    TMPDIRWIN=`cygpath -w ${TMPDIR}`;;
+    *)          TMPDIRWIN=${TMPDIR}
+esac
+
+
+while getopts 'qr:h' opt; do
+  case "$opt" in
+    q)
+      MVNOPTS="-q"
+      CURLOPTS="-s"
+      VERBOSE="0"
+      ;;
+
+    r)
+      arg="$OPTARG"
+      PFLIBVER=${OPTARG}
+      ;;
+   
+    ?|h)
+      echo "This script downloads a proforma java library release from github and"
+      echo "installs it to your local maven repository."
+      echo "All releases can be found here:"
+      echo "  https://github.com/hsh-elc/proforma/releases"
+      echo ""
+      echo "Usage: $(basename $0) [-q] [-r release]"
+      echo "  -h            help"
+      echo "  -q            quiet"
+      echo "  -r release    select release. Default release is $PFLIBVER"
+      exit 1
+      ;;
+  esac
+done
+shift "$(($OPTIND -1))"
+
+
+
+declare -a arrayDownloads=(\
+  proforma-${PFLIBVER}.pom \
+  proformaxml-${PFLIBVER}.pom \
+  proformaxml-2-1-${PFLIBVER}.pom  \
+  proformautil-${PFLIBVER}.pom  \
+  proformautil-2-1-${PFLIBVER}.pom  \
+  proformaxml-${PFLIBVER}.jar \
+  proformaxml-2-1-${PFLIBVER}.jar  \
+  proformautil-${PFLIBVER}.jar  \
+  proformautil-2-1-${PFLIBVER}.jar  \
 )
 
 
 # Working directory:
-mkdir -p target/tmp/download
+WDIR="$TMPDIR/mvnInstallProformaDependenciesFromGithub"
+mkdir -p "$WDIR"
+WDIRWIN="$TMPDIRWIN/mvnInstallProformaDependenciesFromGithub"
 
+
+echoline() {
+    local text=$1
+    if [ $VERBOSE -ne 0 ]; then
+        echo "-------------------------------------------------------------------------------------"
+        echo $text
+        echo "-------------------------------------------------------------------------------------"
+    fi
+}
 
 
 download() {
     local file=$1
-    local url=$2
-    echo "-------------------------------------------------------------------------------------"
-    echo "   downloading from $url to target/tmp/download/$file"
-    echo "-------------------------------------------------------------------------------------"
-    curl --follow \
-        -o target/tmp/download/$file \
+    local url="https://github.com/hsh-elc/proforma/releases/download/v${PFLIBVER}/$file"
+    echoline "   downloading from $url to $WDIR/$file"
+    curl $CURLOPTS --follow \
+        -o "$WDIR/$file" \
         $url
         
-    if [[ $file =~ \.jar$ ]]; then
-        extension="${file##*.}"
-        filename="${file%.*}"
-        rm -vrf "target/tmp/download/${filename}_META-INF"
-        mkdir -v -p "target/tmp/download/${filename}_META-INF"
-        unzip -j -d "target/tmp/download/${filename}_META-INF" target/tmp/download/$file "META-INF/maven/proforma/*"
-        mv -fv "target/tmp/download/${filename}_META-INF/pom.xml" "target/tmp/download/${filename}.pom" 
-        rm -vrf "target/tmp/download/${filename}_META-INF"
-    fi
 }
 
 deploy() {
     local file=$1
-    echo "-------------------------------------------------------------------------------------"
-    echo "  mvn install $file"
-    echo "-------------------------------------------------------------------------------------"
+    echoline "   mvn install $file"
 
     extension="${file##*.}"
     filename="${file%.*}"
 
-    mvn install:install-file \
-      -Dfile="target/tmp/download/$file" \
-      -DpomFile="target/tmp/download/$filename.pom" 
+    mvn $MVNOPTS install:install-file \
+      -Dfile="$WDIRWIN/$file" \
+      -DpomFile="$WDIRWIN/$filename.pom" 
 }
 
 
 
 
-for i in "${!arrayDownloads[@]}"
+for i in "${arrayDownloads[@]}"
 do
-    download "$i" "${arrayDownloads[$i]}"
+    download "$i"
     deploy "$i"
 done
+
+# cleanup
+if [ -d "$WDIR" ]; then
+  rm -rf $WDIR
+fi
